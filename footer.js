77b9e01617979ca412614a1e4db2daddddb2a1b6
andrewtranter <68933121+andrewtranter@users.noreply.github.com>
2021-11-21 15:19:57 +1100

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,31 @@
+/* load in footer, copy paste, or can be a file that is async/defer loaded.
+andrew andy@andyt.co https://github.com/andrewtranter/jQuery_deferred_compat */
+window.addEventListener('DOMContentLoaded', JQgo );
+window.addEventListener('load', JQgo );//both ... because of apple
+
+function JQgo()
+{
+	console.log('JQgo');
+
+	if(jQuery().JQ)
+	{
+		jQuery().JQa++;
+		if(jQuery().JQT) clearTimeout(jQuery().JQT);
+		if(jQuery().JQa < 20) jQuery().JQT = setTimeout(function(){JQgo()},jQuery().JQa*50);
+		return;
+	}
+
+	for(var t in window.JQ)
+	{
+		for(var q of window.JQ[t])
+		{
+			if(t == 'f')window['jQuery'].apply(jQuery,q);
+			else 
+			{
+				var context = jQuery(q[0]);
+				context[t].apply(context,q[1]);
+			}
+		}
+	}
+	window.JQ = [];
+}
